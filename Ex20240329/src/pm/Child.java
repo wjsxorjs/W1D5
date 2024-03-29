3230ab211ddb1b0ea8f274bb6b988df7e6da7671
wjsxorjs <wjsxorjs@naver.com>
2024-03-29 17:00:41 +0900

    JAVA 2024.03.29 0.9

--- Ex20240329/src/pm/Child.java
+++ Ex20240329/src/pm/Child.java
@@ -1,6 +1,9 @@
 package pm;
 
 public class Child extends Parent{
+	// Object부터 자신까지 모든 parents를 불러오기 때문에 메모리가 무거워지고 느려진다.
+	// POJO = Plain Old JAVA Object. < 간단한 자바 객체
+	// Spring이 각광을 받는 이유 (상속을 배제해준다.)
 	
 	public void test() {
 		System.out.println("TEST");
--- Ex20240329/src/pm/Main.java
+++ Ex20240329/src/pm/Main.java
@@ -4,17 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Parent p1 = new Child(); // 이게 가능하네
+		
 		
 		// Child c2 = new Parent(); 이거는 불가능 한데
 		
-		Child c1 = new Child();
 		
-		
+		Object o1 = new Child(); 	// 이것도 가능하다.
+		Parent p1 = new Child(); 	// 이게 가능하네.
+		Child  c1 = new Child();
+				
+
+		System.out.print("c1's value: ");
 		System.out.println(c1.getValue());
+		System.out.println("c1.setValue(150)");
 		c1.setValue(150);
+		System.out.print("c1's value: ");
 		System.out.println(c1.getValue());
-//		p1.test();
+		System.out.print("p1's value: ");
+		System.out.println(p1.getValue());
+		System.out.print("o1's class: ");
+		System.out.println(o1.getClass());
 
 	}
 
