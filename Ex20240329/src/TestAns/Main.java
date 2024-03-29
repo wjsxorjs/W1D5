f9a6c94b6879f9def89e6e0ac19e9b9a560e8ba6
wjsxorjs <wjsxorjs@naver.com>
2024-03-29 14:46:01 +0900

    JAVA 2024.03.29 0.65

---
+++ Ex20240329/src/TestAns/Main.java
@@ -0,0 +1,27 @@
+package TestAns;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.print("검색할 도서명: ");
+		Scanner scanKeyword = new Scanner(System.in);
+		String keyword = scanKeyword.nextLine();
+		
+
+		// 도서관 객체 생성
+		BookLibrary bkLib = new BookLibrary();
+		
+		// 도서관에 책 넣기 (도서관 초기화)
+		bkLib.init();
+		
+		// 검색된 도서들을 문자열로 받는다.
+		String searchResult = bkLib.serachBook(keyword);
+		
+		System.out.println(searchResult);
+
+	}
+
+}
